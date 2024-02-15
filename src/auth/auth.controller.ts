85b79d0fb10d1503333e858baf9eca81a1f5727b
ibrahim hajjar <ibrahim.hajjar@7d.com.kw>
2024-02-15 22:58:25 +0300

    remove non-used imports

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,10 @@
-import { UsersService } from './../users/users.service';
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { UserDto } from 'src/users/user.dto';
+import { UserDto } from 'src/user/user.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(
-    private readonly authService: AuthService,
-    private readonly usersService: UsersService,
-  ) {}
+  constructor(private readonly authService: AuthService) {}
 
   @Post('signup')
   signup(@Body() userDto: UserDto) {
