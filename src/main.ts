dfea468f4846141353bb2c3634281f591266a501
ibrahim hajjar <ibrahim.hajjar@7d.com.kw>
2024-02-17 19:24:38 +0300

    add: improve server validation by whitelist

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   app.useGlobalInterceptors(new ResponseInterceptor());
   await app.listen(4000);
 }
